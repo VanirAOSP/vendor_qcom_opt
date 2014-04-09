1135bd378edf0a304628c1e726b9df76dc252bb4
Jake Weinstein <xboxlover360@gmail.com>
2014-04-08 22:17:07 -0500

    Initial QCOM static add (2.5.18 MSM8974)

---
+++ Android.mk
@@ -0,0 +1,33 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libqc-sqlite
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := libqc-sqlite.a
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libqc-skia
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := libqc-skia.a
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libqc-dalvik
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := libqc-dalvik.a
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libqc-opt
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := libqc-opt.so
+
+include $(BUILD_SHARED_LIBRARY)
